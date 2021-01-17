a256ecc633f63cd76aaf9bf87c446d0f63d4f2db
tpaxman <tpaxman@ualberta.ca>
2021-01-16 22:07:11 -0700

    add pycon videos from 2012-2016

--- list_pycon_talks.py
+++ list_pycon_talks.py
@@ -1,7 +1,7 @@
-import pathlib as pl
-import bs4
-import pandas as pd
 import re
+import bs4
+import pathlib as pl
+import pandas as pd
 
 df_tables = []
 for x in pl.Path('./html-data/').glob('*.html'):
@@ -9,13 +9,21 @@ for x in pl.Path('./html-data/').glob('*.html'):
         html = f.read()
         year = int(re.sub(r'.*-(\d+)', r'\1', x.stem))
         soup = bs4.BeautifulSoup(html, features='lxml')
-        items = [(x['title'], f"https://www.youtube.com{x['href']}") for x in soup.find_all(id='video-title')]
-        df = pd.DataFrame(items)
+        if year > 2013:
+            chunks = soup.find_all(id='video-title')
+            urls = [x['href'] for x in chunks]
+            titles = [x['title'] for x in chunks]
+        else:
+            chunks = soup.find_all(id='content', class_='style-scope ytd-playlist-video-renderer')
+            urls = [x.find('a', class_='yt-simple-endpoint style-scope ytd-playlist-video-renderer')['href'] for x in chunks]
+            titles = [x.find('span', id='video-title')['title'] for x in chunks]
+        items = list(zip(titles, urls)) 
+        df = pd.DataFrame(items).rename(columns={0: 'title', 1: 'url'})
         df['year'] = year
+        df['url'] = "https://www.youtube.com" + df['url']
         df_tables.append(df)
 
-df = pd.concat(df_tables).rename(columns={0: 'title', 1: 'url'})
-df.to_csv('pycon-talks.csv', index=False)
+df = pd.concat(df_tables)
 
 # formatting to markdown style
 df2 = df.copy()
